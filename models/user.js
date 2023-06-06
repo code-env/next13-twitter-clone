754655e893fe261285affa896eebfd958f8c4ae3
code-env <org.codeen@gmail.com>
2023-06-06 12:34:52 +0100

    added some changes in the user model

--- models/user.js
+++ models/user.js
@@ -1,25 +1,44 @@
 import { model, Schema } from "mongoose";
 
-const userSchema = new Schema({
-  username: {
-    type: String,
-    required: [true, "username is required"],
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: [true, "username is required"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    coverpicture: {
+      type: String,
+    },
+    description: {
+      type: String,
+    },
+    followers: [
+      {
+        userId: {
+          type: String,
+        },
+      },
+    ],
+    followings: [
+      {
+        userId: {
+          type: String,
+        },
+      },
+    ],
   },
-  email: {
-    type: String,
-    required: [true, "Email is required"],
-  },
-  password: {
-    type: String,
-    required: [true, "Password is required"],
-  },
-  coverpicture: {
-    type: String,
-  },
-  description: {
-    type: String,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const User = model("user", userSchema);
 
