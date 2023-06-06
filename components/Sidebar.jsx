d6e2bdd6bde5b22046f67f7f3b721a9e6bc340b8
code-env <org.codeen@gmail.com>
2023-06-06 02:47:22 +0100

    removed the useselector from the sidebar

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -4,15 +4,13 @@ import Link from "next/link";
 import { FaTwitter } from "react-icons/fa";
 import { sidebarRoutes } from "@constants";
 import { usePathname } from "next/navigation";
-import { useSelector } from "react-redux";
 
 const Sidebar = () => {
   const pathname = usePathname();
-  const theme = useSelector((state) => state.theme);
 
   return (
     <div
-      className={`w-[20%]  min-h-screen py-4 theme-${theme} bg-skin-fill transition-all duration-300`}
+      className={`w-[20%]  min-h-screen py-4`}
     >
       <div className="flex flex-col gap-8 sticky top-4">
         <div className="icon h-[50px] transition-all w-[50px] rounded-full flex items-center justify-center hover:bg-gray-200/30 cursor-pointer">
