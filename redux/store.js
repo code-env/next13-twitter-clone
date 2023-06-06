b5eabb2db36cf39ff9475b39b2e15641aa2cb5f9
code-env <org.codeen@gmail.com>
2023-06-06 18:28:11 +0100

    removed the themeSlice since it's not still needed

--- redux/store.js
+++ redux/store.js
@@ -1,12 +1,10 @@
 "use client";
 
 import { configureStore } from "@reduxjs/toolkit";
-import themeSlice from "./slice/themeSlice";
 import modalSlice from "./slice/modalSlice";
 
 export const store = configureStore({
   reducer: {
-    theme: themeSlice,
     modal: modalSlice,
   },
 });
